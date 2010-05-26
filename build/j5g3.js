865caea7cc04790cdb09557f6c7d502fb324f0f3
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2010-05-26 19:29:22 -0400

    Added Rotate Action

--- build/j5g3.js
+++ build/j5g3.js
@@ -69,6 +69,17 @@ j5g3.Action = function(properties)
 	this.draw = (typeof properties == 'function') ? properties : properties.code;
 };
 
+/**
+ * Rotates object forever.
+ */
+j5g3.Action.rotate = function(obj)
+{
+	return function() { 
+		var r = obj.rotation();
+		obj.rotation(r < 6.1 ? r+0.1 : 0);
+	};
+}
+
 
 j5g3.Easing =
 {
