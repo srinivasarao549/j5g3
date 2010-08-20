a8210791e51cda613dcc9985c4fc439fbf6a584b
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2010-08-20 16:41:13 -0400

    Added new shortcut for object creation

--- build/j5g3.js
+++ build/j5g3.js
@@ -7,7 +7,7 @@
  * Dual licensed under the MIT or GPL Version 2
  * http://jquery.org/license
  *
- * Date: 2010-08-20 02:12:04 -0400
+ * Date: 2010-08-20 16:36:56 -0400
  */
 
 (function(window, document, undefined) {
@@ -838,18 +838,35 @@ Action.rotate = function(obj)
 	};
 }
 
-    $.Action = Action;
-    $.Animate = Animate;
-    $.Clip   = Clip;
-    $.DisplayObject = DisplayObject;
-    $.Draw = Draw;
-    $.Image = Image;
-    $.Property = Property;
-    $.Range = Range;
-    $.Rect = Rect;
-    $.Sprite = Sprite;
-    $.Spritesheet = Spritesheet;
-    $.Text = Text;
-    $.Util = Util;
+var f = function(klass)
+{
+	return function(properties) { return new klass(properties); }
+};
+
+/* MODULES */
+$.Animate = Animate;
+$.Draw = Draw;
+$.Property = Property;
+$.Util = Util;
+
+/* CLASSES */
+$.Action = Action;
+$.Clip   = Clip;
+$.DisplayObject = DisplayObject;
+$.Image = Image;
+$.Range = Range;
+$.Rect = Rect;
+$.Sprite = Sprite;
+$.Spritesheet = Spritesheet;
+$.Text = Text;
+
+$.action = f(Action);
+$.clip   = f(Clip);
+$.image  = f(Image);
+$.range  = f(Range);
+$.rect   = f(Rect);
+$.sprite = f(Sprite);
+$.spritesheet = f(Spritesheet);
+$.text   = f(Text);
 
 })(this, document);
--- src/outro.js
+++ src/outro.js
@@ -1,15 +1,32 @@
-    $.Action = Action;
-    $.Animate = Animate;
-    $.Clip   = Clip;
-    $.DisplayObject = DisplayObject;
-    $.Draw = Draw;
-    $.Image = Image;
-    $.Property = Property;
-    $.Range = Range;
-    $.Rect = Rect;
-    $.Sprite = Sprite;
-    $.Spritesheet = Spritesheet;
-    $.Text = Text;
-    $.Util = Util;
+var f = function(klass)
+{
+	return function(properties) { return new klass(properties); }
+};
+
+/* MODULES */
+$.Animate = Animate;
+$.Draw = Draw;
+$.Property = Property;
+$.Util = Util;
+
+/* CLASSES */
+$.Action = Action;
+$.Clip   = Clip;
+$.DisplayObject = DisplayObject;
+$.Image = Image;
+$.Range = Range;
+$.Rect = Rect;
+$.Sprite = Sprite;
+$.Spritesheet = Spritesheet;
+$.Text = Text;
+
+$.action = f(Action);
+$.clip   = f(Clip);
+$.image  = f(Image);
+$.range  = f(Range);
+$.rect   = f(Rect);
+$.sprite = f(Sprite);
+$.spritesheet = f(Spritesheet);
+$.text   = f(Text);
 
 })(this, document);
