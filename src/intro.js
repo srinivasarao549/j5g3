af55752f3e28eae4c1abfcf09630d527dd739765
Giancarlo Bellido <giancarlo.bellido@gmail.com>
2010-12-21 12:06:41 -0500

    Changed License to GPLv3

--- src/intro.js
+++ src/intro.js
@@ -2,10 +2,20 @@
  * j5g3 v@VERSION - Javascript Game Engine 
  * http://hackerhosting.com/j5g3
  *
- * Copyright 2010, Giancarlo Bellido
+ * Copyright 2010, Giancarlo F Bellido
  *
- * Dual licensed under the MIT or GPL Version 2
- * http://jquery.org/license
+ * j5g3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * j5g3 is distributed in the hope that it will be useful
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <http://www.gnu.org/licenses/>.
  *
  * Date: @DATE
  */
